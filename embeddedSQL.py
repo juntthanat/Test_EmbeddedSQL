6089ecc2e67bb1c67d540ee7f7649b5797ad45d1
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2022-05-02 09:07:01 +0700

    feat: create a bd

---
+++ embeddedSQL.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+con = sqlite3.connect('buyAHouse.db')
+cur = con.cursor()
+
+# Create table
+cur.execute('''CREATE TABLE house
+            (HNumber, HNAME, PRICE, CITY, FLOORSPACE)''')
+
+# Insert a row of data
+cur.execute("INSERT INTO house VALUES (13, Valet, 5000, Bangkok, 50.65)")
+cur.execute("INSERT INTO house VALUES (14, Hamatsu, 70000, Tokyo, 25.56)")
+cur.execute("INSERT INTO house VALUES (17, Franel, 40000, Paris, 50.34)")
+cur.execute("INSERT INTO house VALUES (19, Havillion, 60000, Singapore, 40.52)")
+cur.execute("INSERT INTO house VALUES (23, Valet, 60000, Moscow, 100.5)")
+
+# Save (commit) the changes
+con.commit()
+con.close()
+
+for row in cur.execute('SELECT * FROM house ORDER BY PRICE'):
+    print(row)
