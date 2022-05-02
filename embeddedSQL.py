c14398ace889d85a32d126a07de308bab9b09ad7
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2022-05-02 11:17:11 +0700

    feat: add more example

--- embeddedSQL.py
+++ embeddedSQL.py
@@ -3,7 +3,28 @@ import sqlite3
 con = sqlite3.connect('buyAHouse.db')
 cur = con.cursor()
 
+print("All Houses")
 for row in cur.execute('SELECT * FROM house ORDER BY PRICE'):
+    print(row)
+
+print("\nHouses that have price < 50000")
+for row in cur.execute('select * from house where price < 50000'):
+    print(row)
+
+print("\nHouse that is in Bangkok")
+for row in cur.execute('select * from house where city = "Bangkok"'):
+    print(row)
+
+print("\nHouses that have V as the first letter")
+for row in cur.execute('select * from house where hname like "V%" order by price'):
+    print(row)
+
+print("\nFloorspace Descending")
+for row in cur.execute('select * from house order by floorspace desc'):
+    print(row)
+
+print("\nThe highest price")
+for row in cur.execute('select * from house where price >= (select max(price) from house)'):
     print(row)
 
 con.close()
